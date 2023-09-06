70651d6580829df1bd77527f605a2bdb4e67cbb5
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2023-09-07 01:03:02 +0700

    hero section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      padding:'16px',
+    },
+    extend: {
+      colors: {
+        darkBlue : "#023047",
+        lightBlue : "#00b4d8"
+      },
+      screens:{
+        '2xl':'1320px',
+      }
+    },
   },
   plugins: [],
 }
