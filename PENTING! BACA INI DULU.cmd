fd53b2d29649cc75f636f0307a658c8dbcbdc617
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2023-09-22 12:39:51 +0700

    fixed finishing

---
+++ PENTING! BACA INI DULU.cmd
@@ -0,0 +1,10 @@
+PASTIKAN DEVICE ANDA TERINSTALL NODE JS
+
+Untuk menjalankan kode ini terlebih dulu:
+1. Install tailwind css dengan mengetikan kode berikut di terminal:
+npm init -y
+npm install -D tailwindcss
+npx tailwindcss init
+
+2. Jalankan Node.js dengan ketik kode berikut diterminal:
+npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
